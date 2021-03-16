33c18fd59a28182311eb81afaa2609687961a04c
Steven Kingston <steven.kingston@antstream.com>
2021-03-16 23:14:13 +0000

    Makes the hook async await

--- src/hooks/useSearchTwitterService/index.ts
+++ src/hooks/useSearchTwitterService/index.ts
@@ -10,24 +10,30 @@ const useSearchTwitterPosts = (searchString: string) => {
   
 const amountToFetch = 10
   useEffect(() => {
-    if(searchString) {
-      setResult({ status: 'loading' });
-      fetch(`http://localhost:8080/twitter-search?q=${searchString}&count=${amountToFetch}'`, {
-      // fetch(`https://yiy212fb2d.execute-api.us-east-1.amazonaws.com/Live/twitter-search?q=${searchString}&count=${amountToFetch}'`, {
-        headers: {
-          'Content-Type': 'application/json',
+    const fetchData = async () => {
+      // You can await here
+        setResult({ status: 'loading' });
+        try{
+          const response = await fetch(`http://localhost:8080/twitter-search?q=${searchString}&count=${amountToFetch}'`, {
+            // fetch(`https://yiy212fb2d.execute-api.us-east-1.amazonaws.com/Live/twitter-search?q=${searchString}&count=${amountToFetch}'`, {
+              headers: {
+                'Content-Type': 'application/json',
+              }
+            })
+          const responseJson = await response.json()
+
+          if(responseJson.errors) throw new Error(responseJson.errors[0] || 'Error making the api request')
+          if(responseJson.statuses) setResult({ status: 'loaded', payload: responseJson.statuses })
+          else setResult({ status: 'loaded', payload: responseJson })
+        } catch(error) {
+          setResult({ status: 'error', error })
         }
-      })
-      .then(response => response.json())
-      .then(response => {
-        console.log(response.errors)
-        if(response.errors) throw new Error(response.errors[0] || 'Error making the api request')
-        if(response.statuses) setResult({ status: 'loaded', payload: response.statuses })
-        else setResult({ status: 'loaded', payload: response })
-      })
-      .catch(error => setResult({ status: 'error', error }));
+      }
+    if(searchString) {
+      fetchData();
     }
   }, [searchString]);
+
   return result;
 };
 
